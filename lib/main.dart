ceb9a08e9e8974bdd1eef32f521406b3c28e5057
payelmiah <payel15-12654@diu.edu.bd>
2023-07-22 12:19:10 +0600

    updated all files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main(){
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Homeform(),
@@ -19,51 +20,95 @@ class _HomeformState extends State<Homeform> {
     return Scaffold(
       appBar: _buildAppbar(),
 
-      body: Form(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.person),
-                  hintText: 'Enter your name',
-                  labelText: 'Name',
-                ),
+      body: _buildform(),
+    );
+  }
+}
+
+
+class _buildform extends StatefulWidget {
+  const _buildform();
+
+  @override
+  State<_buildform> createState() => _buildformState();
+}
+
+
+//body building
+class _buildformState extends State<_buildform> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(Icons.person),
+                hintText: 'Enter your name',
+                labelText: 'Name',
               ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
 
-              TextFormField(
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.phone),
-                  hintText: 'Enter your phone number',
-                  labelText: 'Phone',
-                ),
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(Icons.phone),
+                hintText: 'Enter your phone number',
+                labelText: 'Phone',
               ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
 
-              TextFormField(
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.calendar_today),
-                  hintText: 'Enter your date of birth',
-                  labelText: 'Dob',
-                ),
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(Icons.calendar_today),
+                hintText: 'Enter your date of birth',
+                labelText: 'Dob',
               ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
 
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
 
-                      child: ElevatedButton(
-                        onPressed: (){},
-                        child: Text('Submit'),
-                      ),
-
+                  child: ElevatedButton(
+                    onPressed: (){
+                      if (_formKey.currentState!.validate()) {
+                        // If the form is valid, display a Snackbar.
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Data is in processing.')));
+                      }
+                    },
+                    child: const Text('Submit'),
                   ),
+
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
@@ -71,6 +116,8 @@ class _HomeformState extends State<Homeform> {
 }
 
 
+
+//AppBaR building
 AppBar _buildAppbar(){
   return AppBar(
     elevation: 0,
