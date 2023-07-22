52d963f5f0406c58e40b04b34445a860e9197fba
payelmiah <payel15-12654@diu.edu.bd>
2023-07-22 11:43:15 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -17,26 +17,80 @@ class _HomeformState extends State<Homeform> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.teal[500],
-        leading: Icon(Icons.menu),
-        title: Center(child: Text('Form')),
-        actions: [
+      appBar: _buildAppbar(),
 
-         Container(
-           margin: EdgeInsets.all(15),
-           width: 25,
-           height: 20,
-           child: ClipOval(
-            child: Image.asset('assets/avatar.jpg',
-              fit: BoxFit.cover,
-            ),
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.person),
+                  hintText: 'Enter your name',
+                  labelText: 'Name',
+                ),
+              ),
 
-           ),
-         ),
-        ],
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.phone),
+                  hintText: 'Enter your phone number',
+                  labelText: 'Phone',
+                ),
+              ),
+
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.calendar_today),
+                  hintText: 'Enter your date of birth',
+                  labelText: 'Dob',
+                ),
+              ),
+
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+
+                      child: ElevatedButton(
+                        onPressed: (){},
+                        child: Text('Submit'),
+                      ),
+
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+
+AppBar _buildAppbar(){
+  return AppBar(
+    elevation: 0,
+    backgroundColor: Colors.teal[500],
+    leading: Icon(Icons.menu),
+    title: Center(child: Text('Form')),
+    actions: [
+
+      Container(
+        margin: EdgeInsets.all(15),
+        width: 25,
+        height: 20,
+        child: ClipOval(
+          child: Image.asset('assets/avatar.jpg',
+            fit: BoxFit.cover,
+          ),
+
+        ),
+      ),
+    ],
+  );
+
 }
